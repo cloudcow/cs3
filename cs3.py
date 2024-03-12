896c619a96208c6e458e61634e52f39e8b1a93d0
jos <empty>
2024-03-12 23:53:30 +0100

    + initial commit, base game

---
+++ cs3.py
@@ -0,0 +1,69 @@
+import time
+import random
+dmg = random.randrange(25)
+dmg2 = random.randrange(25)
+totalHealth1 = 100
+totalHealth2 = 100
+totalHealth1 = totalHealth1 - dmg
+totalHealth2 = totalHealth2 - dmg2
+
+print("Welcome to the Official 1v1 Counter-Strike Tournament ")
+while True:
+time.sleep(1)
+print("Loading arena... ")
+time.sleep(1)
+print("Loading arena... ")
+time.sleep(1)
+print("Loading arena... ")
+time.sleep(1)
+print("Loading success! ")
+break
+
+while True:
+print("M4/AK47 ")
+choice = input("Choose a weapon for the players: ")
+if choice == ("M4"):
+print("They are armed with M4s. ")
+break
+if choice == ("AK47"):
+print("They are armed with AK47s. ")
+break
+
+while True:
+time.sleep(1.5)
+print("Okay lets go. ")
+time.sleep(1.5)
+print("The round has started! ")
+break
+
+while True:
+time.sleep(1.5)
+print("The players are going A! ")
+time.sleep(1.5)
+print("... ")
+time.sleep(1.5)
+print("..." )
+break
+
+while True:
+print("The players have met each other on A! ")
+time.sleep(1.5)
+print("The firefight has begun! ")
+break
+
+while True:
+time.sleep(2)
+dmg = random.randrange(25)
+dmg2 = random.randrange(25)
+totalHealth1 = totalHealth1 - dmg
+totalHealth2 = totalHealth2 - dmg2
+print(f"Counter-Terrorist health: {totalHealth1} ")
+print(f"Terrorist health: {totalHealth2}" )
+print("------------------------------")
+
+if totalHealth1 < 1:
+print("Terrorists win! ")
+break
+if totalHealth2 < 1:
+print("Counter-Terrorists win! ")
+break
